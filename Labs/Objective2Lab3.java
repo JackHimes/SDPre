335cc245c1c1bedbdab5e7e42608ba9d9f658f10
Jack H <jack.himes3246@gmail.com>
2021-08-28 12:55:24 -0600

    completed objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,12 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+    num1 = 3;
+    num2 = 4;
+    num3 = 5;
+    double sum = num1 + num2 + num3;
+    average = sum / 3;
+    System.out.println(average);
+
+  }
+}
