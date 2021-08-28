f243fb46d2b88115a88a5d48e987d4cf5a1f19a8
Jack H <jack.himes3246@gmail.com>
2021-08-28 14:12:44 -0600

    completed objective3lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,9 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    double side1, side2, hypotenuse;
+    side1 = 10;
+    side2 = 15;
+    hypotenuse = Math.sqrt(Math.pow(side1, 2) + Math.pow(side2, 2));
+    System.out.println(hypotenuse);
+  }
+}
