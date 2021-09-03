598771313eb0608ba894045ad258e759a915d53a
Jack H <jack.himes3246@gmail.com>
2021-09-03 10:53:41 -0600

    completed objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,12 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    for (int i=0; i < 11; i++) {
+      System.out.println(i);
+    }
+    int i = 10;
+    while (i != 0) {
+      System.out.println(i);
+      i --;
+    }
+  }
+}
